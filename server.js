921fca2b08255b7b86791a610231bfa452b1ca89
spshubham <shubhampatil7675@gmail.com>
2026-09-07 18:57:33 +0530

    Add basic Node.js app and Dockerfile

---
+++ server.js
@@ -0,0 +1,10 @@
+const http = require("node:http");
+
+const server = http.createServer((req, res) => {
+  res.writeHead(200, { "Content-Type": "text/plain" });
+  res.end("Hello from Node.js!\n");
+});
+
+server.listen(3000, () => {
+  console.log("Server running at http://localhost:3000");
+});
